350195cd6c4b7f873445caac4ffeef6eb6f8155c
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2016-10-13 16:15:00 -0300

    Exercicio 1 aparentemente finalizado

--- Exercicio1/script1.sh
+++ Exercicio1/script1.sh
@@ -23,32 +23,42 @@ media_locals=0
 
 media_remotes=0
 
+echo "Requisicoes locais feitas: $locals"
+
+echo "Requisicoes remotas feitas: $remotes"
 
 
 
+
+#Soma das horas das requisicoes feitas tanto remotas quanto locais
 
 while read line;do
     
+        hora=$(echo $line | cut -d":" -f2)
 
-    hora=$(echo $line | cut -d ":" -f2)
+        if [[ $line == *"local"* ]];then
+            media_locals=$(echo "$media_locals+$hora" | bc)
+            echo $media_locals
+        else
+            media_remotes=$(echo "$media_remotes+$hora" | bc)
+            echo $media_remotes      
 
-    if [[ $line == *"local"* ]];then
-       media_locals=$(($media_locals+10#$hora))
-    
-    else
-        media_remotes=$(($media_remotes+10#$hora))
-
-    
-    fi
+       fi
 
     
     
 
 done < calgary_access_log
 
-echo $media_locals
 
-echo $media_remotes
+media_locals=$(echo  "$media_locals/$locals" | bc)
+
+media_remotes=$(echo "$media_remotes/$remotes" | bc)
+
+
+echo "Media das horas locais: $media_locals"
+
+echo "Media das horas remotas: $media_remotes"
 
 
 
