a4dfd923ef1795732c8a6718e678a79511b39e2a
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2016-10-26 23:00:21 -0300

    Exercicio 2 incompleto

--- Exercicio2/script2.sh
+++ Exercicio2/script2.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ $# -le 3 ] ; then
+    N=$1
+    S=$2
+    P_USER=$3
+
+
+
+
+else
 #Captura das entradas
 echo "Digite o numero de observacoes:"
 read N
@@ -15,6 +24,17 @@ echo "Digite o comeco de nome de um usuario:"
 read P_USER
 
 echo
+
+fi
+
+CPU=0
+maior_CPU=0
+
+MEM=0
+maior_MEM=0
+
+
+valor=0
 
 
 #Verificacao dos casos propostos
@@ -39,66 +59,57 @@ echo
     sleep $S
 
 	ps aux | grep ^$P_USER > file.txt
-    
+      if [[ -s $new_file ]] ; then
+   
+
+ 
+        while read line;do
+            valor=$(echo $line | cut -d" " -f3)
+            echo $valor
+
+
+            if [ $(echo "$valor > $maior_CPU" | bc) -eq 1 ] ; then
+              
+                  maior_CPU=$valor
+            fi
+            
+            CPU=$(bc -l <<< "$CPU+$valor")
+
+            valor=$(echo $line | cut -d" " -f4)
+
+            if [ $(echo "$valor > $maior_MEM" | bc) -eq 1 ] ; then
+
+                maior_MEM=$valor
+            fi
+
+            MEM=$(bc -l <<< "$MEM+$valor")
+
+        done < file.txt
+
+    echo "%CPU encontrado: $CPU"
+
+    echo
+
+    echo "Maior %CPU encontrado: $maior_CPU"
+
+    echo
+
+    echo "%MEM encontrada: $MEM"
+
+    echo
+
+    echo "Maior %MEM encontrada: $maior_MEM"
+
+
+    else
+        echo 2
+
+    fi
+
    new_file=file.txt 
 
 
 
 	done
-
-CPU=0
-maior_CPU=0
-
-MEM=0
-maior_MEM=0
-
-
-valor=0
-
-
-    if [[ -s $new_file ]] ; then
-   
-
- 
-        while read line;do
-            valor=$(echo $line | cut -d"$P_USER" -f2)
-            echo $valor
-
-
-            if [ $valor -gt $maior_CPU ] ; then
-              
-                  maior_CPU=$valor
-            fi
-            
-            CPU=$(echo "$CPU+$valor" | bc)
-
-            valor=$(echo $line | cut -d"$P_USER" -f3)
-
-            if [ $valor -gt $maior_MEM ] ; then
-
-                maior_MEM=$valor
-            fi
-
-            MEM=$(echo "$MEM+$valor" | bc)
-
-        done < file.txt
-
-    fi
-
-
-
-echo "%CPU encontrado: $CPU"
-
-echo
-
-echo "Maior %CPU encontrado: $maior_CPU"
-
-echo
-
-echo "%MEM encontrada: $MEM"
-
-echo
-
-echo "Maior %MEM encontrada: $maior_MEM"
 
 
